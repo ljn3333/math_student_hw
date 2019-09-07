02ecc62ba082a163829821211894276d58a57213
kater <31986455+ljn3333@users.noreply.github.com>
2019-09-07 10:31:04 +0800

    Update 54285.py

--- 54285.py
+++ 54285.py
@@ -6,7 +6,7 @@ def division(a , b):
 n = 0        
 for i in range(1,201):
     if (division(i,2) or division(i,3) or division(i,5)) and (not division(i,7)):
-       #print(i)
+       print(i)
        n+=1
        
 print("Question 1   n = ",n) 
@@ -15,7 +15,7 @@ print("Question 1   n = ",n)
 n = 0        
 for i in range(1,201):
     if (division(i,2) or division(i,3) and (not division(i,5))) and (not division(i,7)):
-       #print(i)
+       print(i)
        n+=1
        
 print("Question 2   n = ",n)       
@@ -23,7 +23,7 @@ print("Question 2   n = ",n)
 n = 0        
 for i in range(1,201):
     if (division(i,2) and(not division(i,3)) and (not division(i,5))) and (not division(i,7)):
-       #print(i)
+       print(i)
        n+=1
        
 print("Question 3   n = ",n)       
